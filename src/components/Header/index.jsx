de2531d489254f6e6e2e10e89d6a376103e85647
Mateus Lipinski <mateuslipinskimatias@gmail.com>
2025-09-08 20:50:58 -0300

    feat/init-project

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function Header() { 
+    return(
+        <header>
+            <h1>Avaliação - API Futurama</h1>
+        </header>
+    )
+}
---
+++ src/pages/Random/index.jsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function Random() {
+    return(
+        <main>
+            azeitona
+        </main>
+    )
+}
