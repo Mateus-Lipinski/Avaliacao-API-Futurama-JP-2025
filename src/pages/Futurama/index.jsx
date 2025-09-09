ef141e9d660a9d0867d00a1fe97013b9efe76aec
Mateus Lipinski <mateuslipinskimatias@gmail.com>
2025-09-08 22:01:52 -0300

    feat/api-axios

--- src/pages/Futurama/index.jsx
+++ src/pages/Futurama/index.jsx
@@ -1,9 +1,42 @@
+import { getCharacters } from '../../api/characters'
+import { useState, useEffect } from 'react'
 import './style.css'
 
 export default function Futurama() {
+
+    const [conteudo, setConteudo] = useState(<>Carregando...</>)
+
+    async function TransformaEmLista() {
+
+        const params = { }
+
+        const todosPersonagens = await getCharacters(params)
+
+        return todosPersonagens.map(personagem =>  //.map Não utiliza chaves'{}'
+            <div className='card' key={personagem.id}>
+                <img src={personagem.image} alt={personagem.name}></img>
+                <h2>{personagem.name}</h2>
+                <div>
+                    <p>Espécie: {personagem.species}</p>
+                    <p>Gênero: {personagem.gender}</p>
+                </div>
+            </div>
+        )
+    }
+
+    async function carregar() {
+        setConteudo(await TransformaEmLista())
+    }
+
+    useEffect(() => {
+        carregar()
+    }, [])
+
     return(
         <main>
-            cenoura
+            <div className='container'>
+                {conteudo}
+            </div>
         </main>
     )
 }
--- src/pages/Random/index.jsx
+++ src/pages/Random/index.jsx
@@ -1,9 +1,38 @@
+import { getCharacters } from '../../api/random'
+import { useEffect, useState } from 'react'
 import './style.css'
 
 export default function Random() {
+
+    const [conteudo, setConteudo] = useState(<>Carregando...</>)
+
+    async function TransformaEmLista() {
+
+        const personagem = await getCharacters()
+
+        return <div className='card' key={personagem.id}>
+            <img src={personagem.image} alt={personagem.name}></img>
+            <h2>{personagem.name}</h2>
+            <div>
+                <p>Espécie: {personagem.species}</p>
+                <p>Gênero: {personagem.gender}</p>
+            </div>
+        </div>
+    }
+
+    async function carregar() {
+        setConteudo(await TransformaEmLista())
+    }
+
+    useEffect(() => {
+        carregar()
+    }, [])
+
     return(
         <main>
-            azeitona
+            <div className='container'>
+                {conteudo}
+            </div>
         </main>
     )
 }
